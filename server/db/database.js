4b5ed736e4a52d0374b721ccf8ffee9ea6235c46
Benjamin Pak <benjaminjpak95@gmail.com>
2025-02-21 14:42:05 -0800

    updated database env variables

--- server/db/database.js
+++ server/db/database.js
@@ -1,15 +1,14 @@
 const mysql = require("mysql2/promise");
 
-const dbConnect = mysql.createConnection({
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
-dbConnect
-  .then(() => console.log("Successfully connected to the database!"))
-  .catch((err) =>
-    console.error("There was a problem connecting to the db: ", err)
-  );
+db.then(() => console.log("Successfully connected to the database!")).catch(
+  (err) => console.error("There was a problem connecting to the db: ", err)
+);
 
-module.exports = dbConnect;
+module.exports = db;
